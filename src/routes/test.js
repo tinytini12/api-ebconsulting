d30e05f7c30fa89e049addd3eae7be199167e612
Andrea Agostini <andreaagostini@udd.cl>
2019-09-24 09:41:00 -0300

    API de test

--- src/routes/test.js
+++ src/routes/test.js
@@ -5,13 +5,82 @@ const db = require('../database');
 const limit = 10;
 
 router.get('/test', (req, res) => {
-    db.query('SELECT * from test limit ?', [limit], (error, rows, fields) => {
+    db.query('SELECT * FROM test ORDER BY id_test ASC limit ?', [limit], (error, rows) => {
         if (!error) {
             res.json(rows);
         } else {
-            console.log(error);
+            res.status(500).send({status: 'error', message: 'Error in API'});
         }
     });
+});
+
+router.post('/test', (req, res) => {
+    if (Object.keys(req.body).length > 0 && req.body.name && req.body.name !== ''
+        && req.body.id_course && req.body.id_course !== '' && !isNaN(req.body.id_course)) {
+        const {name, id_course} = req.body;
+        db.query('INSERT INTO test(name, id_course) values(?, ?)', [name, id_course], (error) => {
+            if (!error) {
+                res.json({status: 'ok', message: 'Test created'});
+            } else {
+                res.status(500).send({status: 'error', message: 'Error in insert'});
+            }
+        });
+    } else {
+        res.status(404).send({status: 'error', message: 'Invalid input'});
+    }
+});
+
+router.delete('/test/:id', (req, res) => {
+    if (isNaN(req.params.id)) {
+        res.status(404).send({status: 'error', message: 'Invalid input'});
+    } else {
+        const {id} = req.params;
+
+        db.query('SELECT * FROM test WHERE id_test = ?', [id], (error, rows) => {
+            if (!error) {
+                if (rows.length === 0) {
+                    res.status(404).send({status: 'error', message: 'Test not found'});
+                } else {
+                    db.query('DELETE FROM test WHERE id_test = ?', [id], (error) => {
+                        if (!error) {
+                            res.json({status: 'ok', message: 'Test deleted'});
+                        } else {
+                            res.status(500).send({status: 'error', message: 'Error in delete'});
+                        }
+                    });
+                }
+            } else {
+                res.status(500).send({status: 'error', message: 'Error in delete'});
+            }
+        });
+    }
+});
+
+router.put('/test', (req, res) => {
+    if (Object.keys(req.body).length > 0 && req.body.id_test && req.body.id_test !== '' && !isNaN(req.body.id_test)
+        && req.body.name && req.body.name !== '' && req.body.id_course && req.body.id_course !== '' && !isNaN(req.body.id_course)) {
+        const {id_test, name, id_course} = req.body;
+
+        db.query('SELECT * FROM test WHERE id_test = ?', [id_test], (error, rows) => {
+            if (!error) {
+                if (rows.length === 0) {
+                    res.status(404).send({status: 'error', message: 'Test not found'});
+                } else {
+                    db.query('UPDATE test set name=?, id_course=? WHERE id_test=?', [name, id_course, id_test], (error) => {
+                        if (!error) {
+                            res.json({status: 'ok', message: 'Test updated'});
+                        } else {
+                            res.status(500).send({status: 'error', message: 'Error in put'});
+                        }
+                    });
+                }
+            } else {
+                res.status(500).send({status: 'error', message: 'Error in put'});
+            }
+        });
+    } else {
+        res.status(404).send({status: 'error', message: 'Invalid input'});
+    }
 });
 
 module.exports = router;
