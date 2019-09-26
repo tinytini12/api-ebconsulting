6cfb97c21aaee169283ffd33b47e6ae2aadd022e
Andrea Agostini <andreaagostini@udd.cl>
2019-09-26 11:31:14 -0300

    APIS para asignación de resultados de pruebas

--- src/routes/test.js
+++ src/routes/test.js
@@ -29,6 +29,21 @@ router.get('/test/:id', (req, res) => {
     }
 });
 
+router.get('/test/students/:id', (req, res) => {
+    if (isNaN(req.params.id)) {
+        res.status(404).send({status: 'error', message: 'Invalid input'});
+    } else {
+        const {id} = req.params;
+        db.query('SELECT test.id_test, test.name, st.score, student.id_student, student.name, student.lastname FROM student_test st, test, student WHERE  st.id_test = test.id_test and student.id_student = st.id_student AND st.id_test = ?', [id], (error, rows) => {
+            if (!error) {
+                res.json(rows);
+            } else {
+                res.status(500).send({status: 'error', message: 'Error in API'});
+            }
+        });
+    }
+});
+
 router.post('/test', (req, res) => {
     if (Object.keys(req.body).length > 0 && req.body.name && req.body.name !== ''
         && req.body.id_course && req.body.id_course !== '' && !isNaN(req.body.id_course)) {
@@ -88,6 +103,34 @@ router.put('/test', (req, res) => {
                     db.query('UPDATE test set name=?, id_course=? WHERE id_test=?', [name, id_course, id_test], (error) => {
                         if (!error) {
                             res.json({status: 'ok', message: 'Test updated'});
+                        } else {
+                            res.status(500).send({status: 'error', message: 'Error in put'});
+                        }
+                    });
+                }
+            } else {
+                res.status(500).send({status: 'error', message: 'Error in put'});
+            }
+        });
+    } else {
+        res.status(404).send({status: 'error', message: 'Invalid input'});
+    }
+});
+
+router.put('/test/score', (req, res) => {
+    if (Object.keys(req.body).length > 0 && req.body.id_test && req.body.id_test !== '' && !isNaN(req.body.id_test)
+        && req.body.id_student && req.body.id_student !== '' && !isNaN(req.body.id_student)
+        && req.body.score && req.body.score !== '' && !isNaN(req.body.score)) {
+        const {id_test, id_student, score} = req.body;
+
+        db.query('SELECT * FROM student_test WHERE id_student = ? AND id_test = ?', [id_student, id_test], (error, rows) => {
+            if (!error) {
+                if (rows.length === 0) {
+                    res.status(404).send({status: 'error', message: 'Test not found'});
+                } else {
+                    db.query('UPDATE student_test set score=? WHERE id_student = ? AND id_test = ?', [score, id_student, id_test], (error) => {
+                        if (!error) {
+                            res.json({status: 'ok', message: 'Score updated'});
                         } else {
                             res.status(500).send({status: 'error', message: 'Error in put'});
                         }
